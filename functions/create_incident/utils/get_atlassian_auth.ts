aaafa2d71ccdebf58da80d0d27d3016122fd5bc1
Jim Park <jimp@scribd.com>
2023-04-28 08:28:01 -0700

    delete to use a more simple starter template

--- functions/create_incident/utils/get_atlassian_auth.ts
+++
@@ -1,7 +0,0 @@
-// deno-lint-ignore no-explicit-any
-export function getBasicAuthAtlassian(env: any) {
-  const username = env["ATLASSIAN_USERNAME"];
-  const password = env["ATLASSIAN_API_KEY"];
-  const basicAuth = "Basic " + btoa(username + ":" + password);
-  return basicAuth;
-}
