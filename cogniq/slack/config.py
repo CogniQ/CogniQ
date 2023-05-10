b3195fab81592be52482785426e4a45f3308c55d
Jim Park <jimp@scribd.com>
2023-05-10 14:18:10 -0700

    feat: enable socket mode for slack connector

--- cogniq/slack/config.py
+++ cogniq/slack/config.py
@@ -5,5 +5,6 @@ Config = {
     "SLACK_BOT_TOKEN": os.environ.get("SLACK_BOT_TOKEN"),
     "SLACK_SIGNING_SECRET": os.environ.get("SLACK_SIGNING_SECRET"),
     "PORT": os.environ.get("PORT") or "3000",
-    "SLACK_BOT_ID": None,
+    "APP_ENV": os.environ.get("APP_ENV") or "production",
+    "SLACK_APP_TOKEN": os.environ.get("SLACK_APP_TOKEN"),
 }
